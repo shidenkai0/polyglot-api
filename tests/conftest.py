2eef9a9dc23af364e59cb70fed07df3b457b726c
Mohamed Messaad <hajimenomomo@gmail.com>
2023-06-15 14:26:01 +0200

    chore: remove explicit pytest_asyncio event loop fixture, as it is redundant (pytest-asyncio already instantiates an event loop per test item).

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,3 @@ pytest_plugins = [
     fixture_file.replace("/", ".").replace(".py", "")
     for fixture_file in glob("tests/**/fixtures/[!__]*.py", recursive=True)
 ]
-
-
-@pytest.fixture(autouse=True)
-def _event_loop(event_loop):
-    """Create an instance of the default event loop for each test case."""
-    return event_loop
